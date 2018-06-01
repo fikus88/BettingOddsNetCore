6a00738ed79852b2646ba8e917bedd5678014cd0
Lukasz Sikorski <fikus88@gmail.com>
2018-06-01 23:41:18 +0100

    Add project files.

---
+++ BettingOddsTestASPNETCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BettingOddsTestASPNETCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
